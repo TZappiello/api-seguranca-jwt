101e0a8bfea8b24ec0f8f36ca962ff941f3c1e37
tzappiello <tzappiello@gmail.com>
2026-04-24 17:13:51 -0300

    Refactor: Specify origins for @CrossOrigin in LoginController and UsuarioController

--- src/main/java/com/zap/api/controller/UsuarioController.java
+++ src/main/java/com/zap/api/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin()
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/usuarios")
